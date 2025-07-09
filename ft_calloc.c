dd0486328ce874a3ecf6e34cb20763f8f3c2fcbc
kaahn <oguzkaandemirr@gmail.com>
2025-07-09 15:19:14 +0300

    removed extra check in calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ogudemir <ogudemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 19:36:06 by ogudemir          #+#    #+#             */
-/*   Updated: 2025/07/08 17:29:19 by ogudemir         ###   ########.fr       */
+/*   Updated: 2025/07/09 15:18:46 by ogudemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*a;
 
-	if (size != 0 && count != 0 && count > (size_t) -1 / size)
-		return (NULL);
 	a = malloc(count * size);
 	if (a == NULL)
 		return (NULL);
