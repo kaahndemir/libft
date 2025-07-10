be81e1f59f3817000f53a5a959adc1ed91f53e1d
kaahn <oguzkaandemirr@gmail.com>
2025-07-10 20:36:09 +0300

    fix: renamed the parameters in ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,30 +6,29 @@
 /*   By: ogudemir <ogudemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 19:58:29 by ogudemir          #+#    #+#             */
-/*   Updated: 2025/07/05 19:59:43 by ogudemir         ###   ########.fr       */
+/*   Updated: 2025/07/10 20:32:41 by ogudemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	stackp;
 	size_t	needlp;
 
 	stackp = 0;
 	needlp = 0;
-	if (needle[0] == '\0')
-		return ((char *)haystack);
+	if (little[0] == '\0')
+		return ((char *)big);
 	if (len == 0)
 		return (NULL);
-	while (haystack[stackp] && len > stackp)
+	while (big[stackp] && len > stackp)
 	{
-		while (haystack[stackp + needlp] == needle[needlp] && len > stackp
-			+ needlp)
+		while (big[stackp + needlp] == little[needlp] && len > stackp + needlp)
 		{
-			if (needle[needlp + 1] == '\0')
-				return (&((char *)haystack)[stackp]);
+			if (little[needlp + 1] == '\0')
+				return (&((char *)big)[stackp]);
 			needlp++;
 		}
 		stackp++;
